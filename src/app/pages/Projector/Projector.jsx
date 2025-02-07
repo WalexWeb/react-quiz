0f37052f065444e14a92f656fa8cdbc450a5f02f
Stanislav Pomeshchikov <spomeshchikov@yandex.ru>
2025-02-07 16:17:28 +0300

    add images

--- src/app/pages/Projector/Projector.jsx
+++ src/app/pages/Projector/Projector.jsx
@@ -1,22 +1,34 @@
 import styles from "./Projector.module.scss";
 import AnswerTimer from "../../components/answerTimer/AnswerTimer";
+import { useState } from "react";
 
 function Projector() {
   document.title = "Викторина | Проектор";
 
+  const [seconds, setSeconds] = useState(0);
+
   const handleTimeUp = () => {};
+
+  // Извлекаем время из таймера
+  const extractTime = (second) => {
+    setSeconds(second);
+  };
 
   return (
     <div className={styles.window}>
       <div className={styles.header}>
         <h1 className={styles.chapter}>Раздел</h1>
         <div className={styles.timer}>
-          <AnswerTimer duration={60} onTimeUp={handleTimeUp} />
+          <AnswerTimer
+            time={extractTime}
+            duration={60}
+            onTimeUp={handleTimeUp}
+          />
         </div>
         <p className={styles.question}>Вопрос</p>
       </div>
       <div className={styles.container}>
-        <img src={image} className={styles.image} />
+        <img src="" className={styles.image} />
       </div>
     </div>
   );
