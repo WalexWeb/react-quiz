2c787ba30151ac9dfa78b6b50d4ac6a75829cd17
WalexWeb <spomeshchikov@yandex.ru>
2025-03-03 08:44:34 +0300

    minor edits

--- src/app/pages/Login/Login.jsx
+++ src/app/pages/Login/Login.jsx
@@ -1,5 +1,4 @@
 import styles from "./Login.module.scss";
-import { useNavigate } from "react-router-dom";
 import Input from "../../components/input/Input";
 import Button from "../../components/button/Button";
 import { Link } from "react-router-dom";
